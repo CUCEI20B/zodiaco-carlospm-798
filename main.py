5646112b44eaf6fc881780447663f46c0233d627
Carlos Paredes <carlos.paredes6900@alumnos.udg.mx>
2020-09-30 12:43:06 -0500

    Quite el <else> y puse mayusculas, prueba 02

--- main.py
+++ main.py
@@ -7,42 +7,39 @@ dia = int(input("Día de nacimiento en n enteros:    "))
 mes = int(input("Mes de nacimiento en n enteros:    "))
 
 if (31>= dia >=21 and mes == 3 ) or (1<= dia <=20 and mes ==4):
-    signo = "aries" #1
+    signo = "Aries" #1
 
 elif (30>= dia >=21 and mes == 4 ) or (1<= dia <=20 and mes ==5):
-    signo = "tauro" #2
+    signo = "Tauro" #2
 
 elif (31>= dia >=21 and mes == 5 ) or (1<= dia <=21 and mes ==6):
-    signo = "geminis" #3
+    signo = "Geminis" #3
 
 elif (30>= dia >=22 and mes == 6 ) or (1<= dia <=22 and mes ==7):
-    signo = "cancer" #4
+    signo = "Cáncer" #4
 
 elif (31>= dia >=23 and mes == 7 ) or (1<= dia <=22 and mes ==8):
-    signo = "leo" #5
+    signo = "Leo" #5
 
 elif (31>= dia >=23 and mes == 8 ) or (1<= dia <=22 and mes ==9):
-    signo = "virgo" #6
+    signo = "Virgo" #6
 
 elif (30>= dia >=23 and mes == 9 ) or (1<= dia <=22 and mes ==10):
-    signo = "libra" #7
+    signo = "Libra" #7
 
 elif (31>= dia >=23 and mes == 10 ) or (1<= dia <=22 and mes ==11):
-    signo = "escorpio" #8
+    signo = "Escorpio" #8
 
 elif (30>= dia >=23 and mes == 11 ) or (1<= dia <=21 and mes ==12):
-    signo = "sagitario" #9
+    signo = "Sagitario" #9
 
 elif (31>= dia >=22 and mes == 12 ) or (1<= dia <=20 and mes ==1):
-    signo = "capricornio" #10
+    signo = "Capricornio" #10
 
 elif (31>= dia >=21 and mes == 1 ) or (1<= dia <=18 and mes ==2):
-    signo = "acuario" #11
+    signo = "Acuario" #11
 
 elif (29>= dia >=19 and mes == 2 ) or (1<= dia <=20 and mes ==3):
     signo = "virgo" #12
-
-else:
-    signo = "Error.\n"
 
 print(signo)
