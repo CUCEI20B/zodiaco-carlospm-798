bc91995fa4a2c81b8bd87973f02997b9948b7da2
Carlos Paredes <carlos.paredes6900@alumnos.udg.mx>
2020-09-30 13:24:11 -0500

    Prueba n 06

--- main.py
+++ main.py
@@ -1,43 +1,44 @@
 """
 Carlos Paredes Márquez.
 Signos del Zodiaco.
+:v
 29/09/2020.
 """
 dia = int(input("Día de nacimiento en n enteros:    "))
 mes = int(input("Mes de nacimiento en n enteros:    "))
 
 if (31>= dia >=21 and mes == 3 ) or (1<= dia <=20 and mes ==4):
-    print("Aries") #1
+    print("aries") #1
 
 if (30>= dia >=21 and mes == 4 ) or (1<= dia <=20 and mes ==5):
-    print("Tauro") #2
+    print("tauro") #2
 
 if (31>= dia >=21 and mes == 5 ) or (1<= dia <=21 and mes ==6):
-    print("Geminis") #3
+    print("geminis") #3
 
 if (30>= dia >=22 and mes == 6 ) or (1<= dia <=22 and mes ==7):
-    print("Cáncer") #4
+    print("cancer") #4
 
 if (31>= dia >=23 and mes == 7 ) or (1<= dia <=22 and mes ==8):
-    print("Leo") #5
+    print("leo") #5
 
 if (31>= dia >=23 and mes == 8 ) or (1<= dia <=22 and mes ==9):
-    print("Virgo") #6
+    print("virgo") #6
 
 if (30>= dia >=23 and mes == 9 ) or (1<= dia <=22 and mes ==10):
-    print("Libra") #7
+    print("libra") #7
 
 if (31>= dia >=23 and mes == 10 ) or (1<= dia <=22 and mes ==11):
-    print("Escorpio") #8
+    print("escorpio") #8
 
 if (30>= dia >=23 and mes == 11 ) or (1<= dia <=21 and mes ==12):
-    print("Sagitario") #9
+    print("sagitario") #9
 
 if (31>= dia >=22 and mes == 12 ) or (1<= dia <=20 and mes ==1):
-    print("Capricornio") #10
+    print("capricornio") #10
 
 if (31>= dia >=21 and mes == 1 ) or (1<= dia <=18 and mes ==2):
-    print("Acuario") #11
+    print("acuario") #11
 
 if (29>= dia >=19 and mes == 2 ) or (1<= dia <=20 and mes ==3):
     print("virgo") #12
